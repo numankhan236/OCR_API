eab65bd53b35a76f3cb0c31d21cae28efe37f65b
mishazahid <mishz@hotmail.com>
2023-02-02 11:23:01 +0500

    Updated to URl

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
+
+
+
 import os  
 import json
 import jwt
 from flask import Flask, render_template, request, jsonify
-
+import urllib.request
+import io
 
 # define a folder to store and later serve the images
 UPLOAD_FOLDER = '/static/uploads/'
-
-
 
 # allow files of a specific type
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
@@ -31,43 +33,36 @@ def ocr_core(filename):
     return text
 
 
-
 # function to check the file extension
 def allowed_file(filename):  
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-
 # route and function to handle the upload page
 @app.route('/', methods=['GET', 'POST'])
 def upload_page():  
     if request.method == 'POST':
-        # check if there is a file in the request
-        if 'file' not in request.files:
-            return jsonify({'error': 'No file selected'})
-        file = request.files['file']
-        # if no file is selected
-        if file.filename == '':
-            return jsonify({'error': 'No file selected'})
+        # get the image URL
+        img_url = request.form.get('img_url')
+        if img_url is None:
+            return jsonify({'error': 'No URL selected'})
+        
+        # download the image from the URL
+        img = urllib.request.urlopen(img_url)
+        file = io.BytesIO(img.read())
 
-        if file and allowed_file(file.filename):
+        if file and allowed_file(img_url):
             # call the OCR function on it
             extracted_text = ocr_core(file)
 
             # extract the text and display it
             return jsonify({'message': 'Successfully processed',
                             'extracted_text': extracted_text,
-                            'img_src': UPLOAD_FOLDER + file.filename})
+                            'img_src': img_url})
     elif request.method == 'GET':
         return ""
 
 
 if __name__ == '__main__':  
     app.run(debug = True , port = 5000)
-
-
-
-
-
-
--- testing.py
+++ testing.py
@@ -1,10 +1,25 @@
+
+
 import requests
 
-url = "http://localhost:5000/"
+url = "http://127.0.0.1:5000"
 
+# specify the URL of the image you want to process
+img_url = "https://cdn.pixabay.com/photo/2016/11/18/14/59/image-1835151_960_720.jpg"
 
-image_file = {'file': ('S3.PNG', open('S3.PNG', 'rb'))}
-second_image = {'file': ('.\static\Capture2.PNG', open('.\static\Capture2.PNG', 'rb'))}
-response = requests.post(url,  files=second_image)
+# send the post request to the API endpoint with the image URL
+response = requests.post(url, data={"url": img_url})
 
-print(response.json())
+# check if the request was successful
+if response.status_code == 200:
+    # parse the JSON response from the server
+    result = response.json()
+    print(result)
+
+    # # extract the extracted text from the response
+    # extracted_text = result["extracted_text"]
+    # print("Extracted text:", extracted_text)
+else:
+    # there was an error in the request
+    print("Request failed:", response.content)
+
